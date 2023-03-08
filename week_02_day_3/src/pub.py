45e107b2b36ea7784a63c182382e126a86289508
GPSPic <picard.guillaume47@gmail.com>
2023-03-08 16:02:58 +0000

    complete MVP

--- week_02_day_3/src/pub.py
+++ week_02_day_3/src/pub.py
@@ -1,15 +1,21 @@
+
 class Pub:
     def __init__(self, name, till, drinks):
         self.name = name
         self.till = till
         self.drinks = drinks
 
-    def has_drink(self, drink):
-        if drink in self.drinks:
-            return True
+    # def has_drink(self, drink):
+    #     if drink.name in self.drinks:
+    #         return True
 
     def add_to_till(self, amount):
         self.till += amount
+
+    def find_drink_by_name(self, drink_name):
+        for drink in self.drinks:
+            if drink.name == drink_name:
+                return drink
 
 
 # if cust has money and pub has drink
--- week_02_day_3/tests/pub_test.py
+++ week_02_day_3/tests/pub_test.py
@@ -13,14 +13,23 @@ class TestPub(unittest.TestCase):
         self.pub = Pub("The Drunken Sailor", 0, drinks_list)
         self.customer = Customer("John Lennon", 50)
 
-    def test_pub_has_drink__found(self):
-        has_beer = self.pub.has_drink(self.beer)
-        self.assertTrue(has_beer)
+    # def test_pub_has_drink__found(self):
+    #     has_beer = self.pub.has_drink(self.beer)
+    #     self.assertTrue(has_beer)
 
     def test_add_to_till(self):
         self.pub.add_to_till(10)
         self.assertEqual(10, self.pub.till)
 
-    def test_sell_a_drink(self):
+    def test_find_drink_by_name(self):
+        drink = self.pub.find_drink_by_name("Beer")
+        self.assertEqual("Beer", drink.name)
 
-
+    def test_sell_a_drink__sucess(self):
+        drink = self.pub.find_drink_by_name("Beer")
+        price = drink.price
+        if self.customer.can_pay_for_drink(price):
+            self.customer.reduce_wallet(price)
+            self.pub.add_to_till(price)
+        self.assertEqual(4, self.pub.till)
+        self.assertEqual(46, self.customer.wallet)
